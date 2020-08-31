d8b8b446633b17d600158c23f6ce61961f1253ff
DYKim <lavumi@flysher.net>
2020-08-31 19:13:31 +0900

    exportjson to json

--- src/DownloadCurrent.js
+++ src/DownloadCurrent.js
@@ -6,13 +6,24 @@ var g_fileContext ="nothing :)";
 
 $('#DownloadBtn').click( function(){
     console.log( 'Download Button is under construction');
- //   download(g_fileName,g_fileContext);
+    //download(g_fileName,g_fileContext);
 }.bind(this));
 
 function download(filename, text) {
     var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
+
+    var jsonStr = convertToJSON(text);
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(jsonStr));
+
+    var ext = cc.path.extname(filename).toLowerCase();
+    var jsonName = "";
+    if ( ext === '.exportjson'){
+        jsonName = filename.replace('ExportJson', 'json');
+    }
+    else {
+        jsonName = filename;
+    }
+    element.setAttribute('download', jsonName);
 
     element.style.display = 'none';
     document.body.appendChild(element);
@@ -22,3 +33,7 @@ function download(filename, text) {
     document.body.removeChild(element);
 }
 
+function convertToJSON(exportJson){
+    var replaced_str = exportJson;//.replace(/,/g, ',\n');
+    return replaced_str;
+}
