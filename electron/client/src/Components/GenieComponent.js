e13208467453474ebc78e9d29a325b79a86d84ec
cswq327 <ybj80@flysher.net>
2024-02-20 09:33:27 +0900

    remove inspectorComponet.js

--- electron/client/src/Components/GenieComponent.js
+++ electron/client/src/Components/GenieComponent.js
@@ -1,5 +1,6 @@
 var Genie = Genie || {};
 Genie.Component = Genie.Component || {};
+
 Genie.Component.Base = cc.Component.extend({
     ctor : function () {
         this._super();
@@ -7,5 +8,17 @@ Genie.Component.Base = cc.Component.extend({
 
     onEnter : function () {
         this._super();
+    }
+});
+
+Genie.Component.InspectorBase = Genie.Component.Base.extend({
+
+    // 인스펙터에 그리고 싶으면 한다.
+    drawInspector : function() {
+        throw Error('Do Override ');
+    },
+
+    setInspectorValue : function() {
+        throw Error('Do Override ');
     }
 });
