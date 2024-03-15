ffba46c70989f0b2d9470ba7feccf2e2d2c0e2ed
taegyunhan <taegyun.han@playlinks.com>
2024-03-15 14:21:06 +0900

    feat: Init sentry

--- electron/renderer_main.js
+++ electron/renderer_main.js
@@ -1,4 +1,10 @@
 const { ipcRenderer } = require('electron');
+const sentryRenderer = require('@sentry/electron/renderer');
+
+sentryRenderer.init({
+    dsn : 'https://84d805b8c03d8b956113b4e8567cad0d@o4506908221112320.ingest.us.sentry.io/4506908226813952'
+});
+
 class FileEntry {
 
     constructor(fileInfo) {
